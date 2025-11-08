6ee45a513946eee34a8a4bc32af27cf271a462c8
Ilinca Mares <ilinca.mares@stud.ubbcluj.ro>
2025-11-08 20:47:15 +0200

    chore(devops): initial setup for SpringBoot backend

---
+++ VibeCheckBE/src/test/java/com/zeroping/vibecheckbe/VibeCheckBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeroping.vibecheckbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeCheckBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
