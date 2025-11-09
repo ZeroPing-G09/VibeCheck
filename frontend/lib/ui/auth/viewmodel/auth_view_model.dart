d00fba4a7aa63cd75b87ec10291d4fb4cbec8ead
Andreea410 <andreeaungur494@yahoo.com>
2025-11-10 01:01:30 +0200

    feat: Implemented the dashboard + my profile views

--- frontend/lib/ui/auth/viewmodel/auth_view_model.dart
+++ frontend/lib/ui/auth/viewmodel/auth_view_model.dart
@@ -1,9 +1,24 @@
-import 'package:frontend/data/repositories/auth_repository.dart';
+import 'package:flutter/foundation.dart';
+import '../../../data/models/user.dart';
+import '../../../data/repositories/auth_repository.dart';
+import '../../../di/locator.dart';
 
-class AuthViewModel {
-  final AuthRepository _repo = AuthRepository();
+class AuthViewModel extends ChangeNotifier {
+  final AuthRepository _authRepository = locator<AuthRepository>();
 
-  Future<void> loginWithSpotify() => _repo.signInWithSpotify();
-  Future<void> logout() => _repo.signOut();
-  Stream get onAuthStateChange => _repo.onAuthStateChange;
+  bool _isLoading = false;
+  UserModel? _user;
+
+  bool get isLoading => _isLoading;
+  UserModel? get user => _user;
+
+  Future<void> login(String email, String password) async {
+    _isLoading = true;
+    notifyListeners();
+
+    _user = await _authRepository.login(email, password);
+
+    _isLoading = false;
+    notifyListeners();
+  }
 }
