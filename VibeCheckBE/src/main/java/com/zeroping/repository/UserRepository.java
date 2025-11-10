26fe75a3a6f874f093c30878f7818473063f660c
dosqas <sebastian.soptelea1@stud.ubbcluj.ro>
2025-11-10 18:34:00 +0200

    changed user repository to extend jparepository

--- VibeCheckBE/src/main/java/com/zeroping/repository/UserRepository.java
+++ VibeCheckBE/src/main/java/com/zeroping/repository/UserRepository.java
@@ -1,34 +1,9 @@
 package com.zeroping.repository;
 
-import com.zeroping.dto.UserPreferencesDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import com.zeroping.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
-    private final JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public UserRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public int updatePreferences(Integer userId, UserPreferencesDTO preferences) {
-        final String SQL = """
-            UPDATE users
-            SET top1_genre_id = ?,
-                top2_genre_id = ?,
-                top3_genre_id = ?
-            WHERE id = ?
-            """;
-        
-        return jdbcTemplate.update(
-            SQL,
-            preferences.getTop1GenreId(), 
-            preferences.getTop2GenreId(), 
-            preferences.getTop3GenreId(), 
-            userId
-        );
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
 }
