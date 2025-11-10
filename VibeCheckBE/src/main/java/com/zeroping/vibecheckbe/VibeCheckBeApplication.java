197bcdf4858ef6d7e230a8a6c2d47040220cb7fe
dosqas <sebastian.soptelea1@stud.ubbcluj.ro>
2025-11-10 17:31:45 +0200

    style: removed some empty lines

--- VibeCheckBE/src/main/java/com/zeroping/vibecheckbe/VibeCheckBeApplication.java
+++ VibeCheckBE/src/main/java/com/zeroping/vibecheckbe/VibeCheckBeApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 // remove exclude after connecting to db
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class VibeCheckBeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VibeCheckBeApplication.class, args);
     }
-
 }
