9a3e8c4322c1e33f8f017ffcf8e4cadf23091830
dosqas <sebastian.soptelea1@stud.ubbcluj.ro>
2025-11-10 18:33:25 +0200

    added the user entity

---
+++ VibeCheckBE/src/main/java/com/zeroping/entity/User.java
@@ -0,0 +1,39 @@
+package com.zeroping.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "Users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "profile_picture")
+    private String profilePicture;
+
+    @Column(name = "top1_genre_id")
+    private Long top1GenreId;
+
+    @Column(name = "top2_genre_id")
+    private Long top2GenreId;
+
+    @Column(name = "top3_genre_id")
+    private Long top3GenreId;
+}
