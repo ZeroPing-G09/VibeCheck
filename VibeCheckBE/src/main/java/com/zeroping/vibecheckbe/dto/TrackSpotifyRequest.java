cc1ee9698836f5cdff0d7a573cce9eac8b90dda0
Ilinca Mares <ilinca.mares@stud.ubbcluj.ro>
2025-11-27 02:39:27 +0200

    feat(backend): integrated SpotifyPlaylistService into PlaylistController , added more error handling to GeminiPlaylistService

--- VibeCheckBE/src/main/java/com/zeroping/vibecheckbe/dto/TrackSpotifyRequest.java
+++ VibeCheckBE/src/main/java/com/zeroping/vibecheckbe/dto/TrackSpotifyRequest.java
@@ -1,8 +1,10 @@
 package com.zeroping.vibecheckbe.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class TrackSpotifyRequest {
     private String title;
     private String artist;
